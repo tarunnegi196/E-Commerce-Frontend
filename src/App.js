32129fe81ae8db1da400b41bab13c1b0ecef134d
tarunnegi196 <tarunnegi402@gmail.com>
2023-05-10 12:50:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import MacBookPro162 from './Components/MacBookPro162';
+function App() {
+  return (
+    <>
+    <MacBookPro162/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MacBookPro162.js
@@ -0,0 +1,211 @@
+import React from 'react'
+
+
+
+import styles from "./MacBookPro162.module.css";
+const MacBookPro162 = () => {
+  return (
+    <div className={styles.macbookPro162}>
+      <img className={styles.macbookPro162Child} alt="img not found" src="/imgg.png" />
+      <b className={styles.gameOn}>Game On</b>
+      <div className={styles.newArrivals}>New Arrivals</div>
+      <div className={styles.newArrivals1}>New Arrivals</div>
+      <div className={styles.newArrivals2}>New Arrivals</div>
+      <img
+        className={styles.materialSymbolsgardenCartOIcon}
+        alt=""
+        src="/imgg.png"
+      />
+      <img className={styles.rimenu3FillIcon} alt="img not found" src="/imgg.png" />
+      <div className={styles.homeParent}>
+        <div className={styles.home}>Home</div>
+        <img className={styles.frameIcon} alt="img not found" src="/imgg.png" />
+        <div className={styles.home}>Football</div>
+        <img className={styles.frameIcon} alt="" src="/imgg.png" />
+        <div className={styles.home}>Boots</div>
+        <img className={styles.frameIcon} alt="" src="/imgg.png" />
+        <div className={styles.home}>Puma</div>
+        <img className={styles.frameIcon} alt="" src="/imgg.png" />
+      </div>
+      <div className={styles.frameParent}>
+        <div className={styles.pumaEvospeedParent}>
+          <div className={styles.pumaEvospeed}>Puma EvoSpeed</div>
+          <div className={styles.puma1}>Puma</div>
+        </div>
+        <div className={styles.frameGroup}>
+          <div className={styles.frameContainer}>
+            <img className={styles.frameIcon4} alt="" src="/imgg.png" />
+            <div className={styles.pumaEvospeed}>109</div>
+          </div>
+          <div className={styles.frameWrapper}>
+            <img className={styles.frameIcon4} alt="" src="/imgg.png" />
+          </div>
+          <div className={styles.frameWrapper}>
+            <img className={styles.frameIcon4} alt="" src="/imgg.png" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.frameDiv}>
+        <div className={styles.parent}>
+          <b className={styles.home}>$71.56</b>
+          <div className={styles.div1}>$71.56</div>
+        </div>
+        <div className={styles.frameParent1}>
+          <div className={styles.groupParent}>
+            <div className={styles.frameParent3}>
+              <img className={styles.frameIcon} alt="" src="/imgg.png" />
+              <div className={styles.pumaEvospeed}>4.8</div>
+            </div>
+            <div className={styles.frameParent4}>
+              <img className={styles.frameIcon} alt="" src="/imgg.png" />
+              <div className={styles.pumaEvospeed}>67 Reviews</div>
+            </div>
+          </div>
+          <div className={styles.ofBuyersHaveContainer}>
+            <span className={styles.span}>93%</span>
+            <span className={styles.ofBuyersHave}>
+              {" "}
+              of buyers have recommended this.
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className={styles.macbookPro162Item} />
+      <div className={styles.macbookPro162Inner} />
+      <div className={styles.lineDiv} />
+      <div className={styles.macbookPro162Child1} />
+      <img className={styles.frameIcon9} alt="" src="/imgg.png" />
+      <img className={styles.frameIcon10} alt="" src="/imgg.png" />
+      <div className={styles.chooseAColorParent}>
+        <div className={styles.chooseAColor}>Choose a Color</div>
+        <div className={styles.groupParent}>
+          <div className={styles.rectangleParent}>
+            <div className={styles.groupChild} />
+            <img
+              className={styles.vectorStrokeIcon}
+              alt="img not found"
+              src="/imgg.png"
+            />
+            <div className={styles.groupItem} />
+          </div>
+          <div className={styles.rectangleParent}>
+            <div className={styles.groupInner} />
+            <div className={styles.rectangleDiv} />
+          </div>
+          <div className={styles.rectangleParent}>
+            <div className={styles.groupChild1} />
+            <div className={styles.rectangleDiv} />
+          </div>
+          <div className={styles.rectangleParent}>
+            <div className={styles.groupChild3} />
+            <div className={styles.rectangleDiv} />
+          </div>
+          <div className={styles.rectangleParent}>
+            <div className={styles.groupChild5} />
+            <div className={styles.rectangleDiv} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.chooseASizeParent}>
+        <div className={styles.chooseAColor}>Choose a Size</div>
+        <div className={styles.frameParent5}>
+          <div className={styles.groupContainer}>
+            <div className={styles.rectangleWrapper}>
+              <div className={styles.groupChild7} />
+              <div className={styles.groupChild8} />
+            </div>
+            <div className={styles.chooseAColor}>Small</div>
+          </div>
+          <div className={styles.groupParent1}>
+            <div className={styles.rectangleWrapper}>
+              <div className={styles.groupChild9} />
+            </div>
+            <div className={styles.chooseAColor}>Medium</div>
+          </div>
+          <div className={styles.groupParent1}>
+            <div className={styles.rectangleWrapper}>
+              <div className={styles.groupChild9} />
+            </div>
+            <div className={styles.chooseAColor}>Large</div>
+          </div>
+          <div className={styles.groupParent1}>
+            <div className={styles.rectangleWrapper}>
+              <div className={styles.groupChild9} />
+            </div>
+            <div className={styles.chooseAColor}>Extra Large</div>
+          </div>
+          <div className={styles.groupParent1}>
+            <div className={styles.rectangleWrapper}>
+              <div className={styles.groupChild9} />
+            </div>
+            <div className={styles.chooseAColor}>XXL</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.groupParent5}>
+        <div className={styles.rectangleParent3}>
+          <div className={styles.groupChild13} />
+          <b className={styles.b1}>1</b>
+          <b className={styles.b2}>+</b>
+          <b className={styles.b3}>-</b>
+        </div>
+        <div className={styles.rectangleParent4}>
+          <div className={styles.groupChild14} />
+          <div className={styles.frameParent6}>
+            <img className={styles.frameIcon11} alt="" src="/imgg.png" />
+            <div className={styles.pumaEvospeed}>Add To Cart</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.frameParent7}>
+        <div className={styles.frameParent8}>
+          <img className={styles.frameIcon12} alt="" src="/imgg.png" />
+          <div className={styles.freeDeliveryParent}>
+            <b className={styles.freeDelivery}>Free Delivery</b>
+            <div className={styles.enterYourPostal}>
+              Enter your Postal code for Delivery Availability
+            </div>
+          </div>
+        </div>
+        <div className={styles.frameChild} />
+        <div className={styles.frameParent8}>
+          <img className={styles.frameIcon12} alt="" src="/imgg.png" />
+          <div className={styles.freeDeliveryParent}>
+            <b className={styles.freeDelivery}>Return Delivery</b>
+            <div className={styles.free30DaysContainer}>
+              {`Free 30 days Delivery Return. `}
+              <span className={styles.details}>Details</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <img
+        className={styles.unsplash27vd8dvyoiIcon}
+        alt=""
+        src="/imgg.png"
+      />
+      <img
+        className={styles.unsplash27vd8dvyoiIcon1}
+        alt=""
+        src="/imgg.png"
+      />
+      <img
+        className={styles.unsplash27vd8dvyoiIcon2}
+        alt=""
+        src="/imgg.png"
+      />
+      <img
+        className={styles.unsplash27vd8dvyoiIcon3}
+        alt=""
+        src="/imgg.png"
+      />
+      <img
+        className={styles.unsplash27vd8dvyoiIcon4}
+        alt=""
+        src="/imgg.png"
+      />
+    </div>
+  );
+};
+
+export default MacBookPro162;
